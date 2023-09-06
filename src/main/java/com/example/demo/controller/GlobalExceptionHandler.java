e027bcdcc9e212a448fa1d0731179823dd13a0dd
K1MTAEHONG <129834529+K1MTAEHONG@users.noreply.github.com>
2023-09-06 20:03:30 +0900

    modify

---
+++ src/main/java/com/example/demo/controller/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public String handleNotValidException(Model model, MethodArgumentNotValidException ex) {
+
+        if (ex.getFieldError() != null) {
+            model.addAttribute("message", ex.getFieldError().getDefaultMessage());
+        }
+
+        return "error";
+    }
+}
