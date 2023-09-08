700155cc7d7f99212f591098165037d918e1a8f6
K1MTAEHONG <129834529+K1MTAEHONG@users.noreply.github.com>
2023-09-08 15:47:59 +0900

    modify

--- src/main/java/com/example/demo/validator/PreventFuxxValidator.java
+++ src/main/java/com/example/demo/validator/PreventFuxxValidator.java
@@ -6,10 +6,9 @@ import jakarta.validation.ConstraintValidatorContext;
 import java.util.Arrays;
 import java.util.List;
 
-public class PreventFuxxValidator implements ConstraintValidator<Author,String> {
+public class PreventFuxxValidator implements ConstraintValidator<PreventFuxx,String> {
 
     List<String> fuxxList = Arrays.asList("씨발","시팔","fuck");
-
 
     @Override
     public boolean isValid(String field, ConstraintValidatorContext constraintValidatorContext){
