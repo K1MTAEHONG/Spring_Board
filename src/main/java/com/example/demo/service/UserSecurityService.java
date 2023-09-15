e633aba037c366967423fed864dc25d9fda7929d
K1MTAEHONG <129834529+K1MTAEHONG@users.noreply.github.com>
2023-09-15 10:12:18 +0900

    modify

--- src/main/java/com/example/demo/service/UserSecurityService.java
+++ src/main/java/com/example/demo/service/UserSecurityService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.model.LoginUser;
+import com.example.demo.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,23 +20,29 @@ import java.util.List;
 public class UserSecurityService implements UserDetailsService {
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @Override
     public UserDetails loadUserByUsername (String username) throws UsernameNotFoundException {
 
-        //username 으로 USER DB 조회
+        // DB에서 id라는 유저를 조회
+        // TODO: 만드예정임
+        UserModel user = userService.selectUserByUserId(username);
+
+        if(ObjectUtils.isEmpty(user)){
+            throw new UsernameNotFoundException("없는 사용자입니다.");
+        }
 
         List<GrantedAuthority> authorities = new ArrayList<>();
 
-        if("admin".equals(username)){
+        if("admin".equals(user.getUserId())){
             authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
             authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
         } else {
             authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-        }
 
-        return new LoginUser(username, passwordEncoder.encode("1234"),"사용자이름", authorities);
+        }
+        return new User(user.getUserId(),user.getPassword(),authorities);
 
     }
 }
